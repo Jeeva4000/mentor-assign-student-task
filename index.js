6939c36cdde5268d9c4015ec08c24e766895a423
Jeeva4000 <vjeeva@gmail.com>
2023-08-07 18:10:36 +0530

    mentor changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
  const db = require("./db.js")
-const mentorstudentrouter = require("./mentorstudentrouter"); 
+const MentorStudentRouter = require("./MentorStudentRouter.js"); 
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/mentors", mentorstudentrouter);
+app.use("/mentors", MentorStudentRouter);
 
 app.listen(3030, () => console.log("server running in localhost:3030"));
