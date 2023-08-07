dad8fd02a34e3635f7b5950b537b570c9ab68848
Jeeva4000 <vjeeva@gmail.com>
2023-08-07 15:22:17 +0530

    new change

--- index.js
+++ index.js
@@ -14,11 +14,11 @@
 // app.listen(3030,()=>console.log("server running in localhost:3030"))
 
 const express = require("express");
-const mentorStudentRouter = require("./mentorstudentrouter"); // Corrected the import path
+const mentorstudentrouter = require("./mentorstudentrouter"); // Corrected the import path
 const app = express();
 
 app.use(express.json());
 
-app.use("/mentors", mentorStudentRouter);
+app.use("/mentors", mentorstudentrouter);
 
 app.listen(3030, () => console.log("server running in localhost:3030"));
