277ce57bc87aeb4be46c8a87255dbe5923619ba1
Jeeva4000 <vjeeva@gmail.com>
2023-08-07 12:47:28 +0530

    new changes

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import { mentorsStudentRouter } from './mentorstudentrouter.js';
 import { createDB } from "./db.js";
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; 
+
 
 app.use(express.json());
 
